f913c4d0b9f4646013e3e1d21e2eb8f498c084d5
samcsf <samfc@me.com>
2019-07-14 23:34:35 +0800

    chore: temporary give up

--- pkg/lock/mutex.go
+++ pkg/lock/mutex.go
@@ -1,3 +1,4 @@
+// 知识层面不够，暂时放弃，想复现饥饿模式的变现
 package main
 
 import (
@@ -11,31 +12,39 @@ func main() {
 	// when in normal mode new coming goroutine will competed with waiting goroutine
 	// but if a goroutine wait more than 1ms, it will change to starve mode
 	// when in starve mode the waiter will get the lock in order
-	var m sync.Mutex
-	go lockItWithLongTime(m)
+	m := &sync.Mutex{}
 	var wg sync.WaitGroup
 	wg.Add(10)
+	start := time.Now()
+	// just make sure all the compete codes are ready
+	go lockItWithLongTime(m)
 	for i := 0; i < 10; i++ {
-		go grabTheLock(i, m, wg)
+		go grabTheLock(i, m, &wg)
 	}
+	m.Lock()
+	// <-time.After(500000 * time.Nanosecond)
+	<-time.After(2 * time.Millisecond)
+	fmt.Println("???")
+	m.Unlock()
 	wg.Wait()
+	fmt.Println(time.Now().Sub(start).String())
 }
 
-func lockItWithLongTime(m sync.Mutex) {
-	m.Lock()
-	defer m.Unlock()
-	<-time.After(100 * time.Millisecond)
+func lockItWithLongTime(m *sync.Mutex) {
+	(*m).Lock()
+	defer (*m).Unlock()
+	<-time.After(1000 * time.Millisecond)
 }
 
-func lockItWithShortTime(m sync.Mutex) {
-	m.Lock()
-	defer m.Unlock()
-	<-time.After(8e5 * time.Nanosecond)
+func lockItWithShortTime(m *sync.Mutex) {
+	(*m).Lock()
+	defer (*m).Unlock()
+	<-time.After(2e5 * time.Nanosecond)
 }
 
-func grabTheLock(id int, m sync.Mutex, wg sync.WaitGroup) {
-	m.Lock()
-	defer m.Unlock()
+func grabTheLock(id int, m *sync.Mutex, wg *sync.WaitGroup) {
+	(*m).Lock()
+	defer (*m).Unlock()
 	fmt.Printf("Id: %d grab the lock\n", id)
-	wg.Done()
+	(*wg).Done()
 }
