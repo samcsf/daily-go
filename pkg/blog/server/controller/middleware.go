34c8db4cea299fe1e76139f3e110aa70c2223d02
samcsf <samfc@me.com>
2019-07-15 23:47:06 +0800

    feat: pkg blog, hack ResposeWriter to record the status code

--- pkg/blog/server/controller/middleware.go
+++ pkg/blog/server/controller/middleware.go
@@ -8,14 +8,26 @@ import (
 
 type Middlewares struct{}
 
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
 func (mdw *Middlewares) Duration(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		defer func() {
-			end := time.Now()
-			log.Printf("| %dms | %s %s", end.Sub(start).Nanoseconds()/1e6, r.Method, r.URL.Path)
-		}()
-		next(w, r)
+
+		// hack to save the status code
+		rec := &statusRecorder{w, 200}
+		next(rec, r)
+
+		end := time.Now()
+		log.Printf("| %dms | %d | %s %s ", end.Sub(start).Nanoseconds()/1e6, rec.status, r.Method, r.URL.Path)
 		return
 	}
 }
