baa2294c2144f2ea838075777860eca3ec03f83b
samcsf <samfc@me.com>
2019-08-10 23:55:00 +0800

    feat: simple web assembly example

---
+++ pkg/web-assembly/hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	// "syscall/js"
+	"log"
+)
+
+func main() {
+	log.Println("Hello world!")
+}
--- pkg/web-assembly/main.go
+++ pkg/web-assembly/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.Handle("/", wasmHanlerMid(http.FileServer(http.Dir("./pkg/web-assembly/html/"))))
+	log.Println("Server start listening 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+// Go在web-assembly中其实就是取代js的位置，服务端所需要做的就是设置正确的content-type让浏览器识别
+func wasmHanlerMid(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, ".wasm") {
+			w.Header().Set("Content-type", "application/wasm")
+		}
+		h.ServeHTTP(w, r)
+	})
+}
